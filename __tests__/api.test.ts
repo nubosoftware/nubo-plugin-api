c362f3855cc2ad92040e4e8b30ac8a42bfac7bb2
Nubo Developer <dev@nubosoftware.com>
2023-01-05 09:26:14 +0200

    Fix test for new plugin init

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,5 +1,6 @@
 import { CoreModule, PluginModule, Server, Login, GetLogin } from "../src/index";
 import { Logger } from "../src/common";
+import { PluginInitResponse } from "../src/pluginModule";
 import { Request, Response } from "../src/server";
 
 
@@ -9,10 +10,22 @@ class MyModule implements PluginModule {
     logger!: Logger;
     initizalized = false;
 
-    init(_coreModule: CoreModule) {
+    init(_coreModule: CoreModule) : PluginInitResponse  {
         this.coreModule = _coreModule;               
         this.logger = this.coreModule.Common.getLogger("MyTestModule");
         this.initizalized = true;
+        return {
+            publicServerHandlers: [
+                {
+                    method: "get",
+                    path: "/mytest",
+                    handler: (req: Request, res: Response) => {
+                        this.logger.info("[MyTestModule] Got request to /mytest");
+                        res.send("Hello World!");
+                    }
+                }
+            ]
+        }
         this.logger.info("[MyTestModule] initialized!");
     }
     deinit() {
@@ -22,13 +35,13 @@ class MyModule implements PluginModule {
             this.logger.info("[MyTestModule] deinitialized!");
         }
     }
-    addPublicServerHandlers(server: Server) {
-        this.logger.info("[MyTestModule] Adding public server handlers!");
-        server.get("/mytest", (req, res) => {
-            this.logger.info("[MyTestModule] Got request to /mytest");
-            res.send("Hello World!");
-        });
-    }
+    // addPublicServerHandlers(server: Server) {
+    //     this.logger.info("[MyTestModule] Adding public server handlers!");
+    //     server.get("/mytest", (req, res) => {
+    //         this.logger.info("[MyTestModule] Got request to /mytest");
+    //         res.send("Hello World!");
+    //     });
+    // }
 
     handleRestApiRequest(objectType: string, arg1: string, arg2: string, arg3: string, perms: any, adminLogin: Login, req: Request, res: Response): boolean {
         this.logger.info("[MyTestModule] Got request to /restapi/" + objectType + "/" + arg1 + "/" + arg2 + "/" + arg3);
